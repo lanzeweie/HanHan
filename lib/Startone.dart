06bc1205bfd1a16152a53b9a95a181e2ef1bdc8b
lanzeweie@ <lanzeweie@foxmail.com>
2023-08-27 12:25:09 +0800

    初始化项目

---
+++ lib/Startone.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'main.dart';
+
+void main() => runApp(const First_launch());
+
+class First_launch extends StatelessWidget {
+  const First_launch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
+    return MaterialApp(
+      title: 'Introduction screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const OnBoardingPage(),
+    );
+  }
+}
+
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  OnBoardingPageState createState() => OnBoardingPageState();
+}
+
+class OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => CardApp()),
+    );
+  }
+
+  Widget _buildFullscreenImage() {
+    return Image.asset(
+      'assets/niao.png',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      allowImplicitScrolling: true,
+      autoScrollDuration: 6000,
+      infiniteAutoScroll: true,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+          ),
+        ),
+      ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            '让我们开始吧！',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "涵涵的超级命令面板",
+          body:
+              "这个命令面板主要提供局域网内的设备控制，当前需要设备上有相应的服务端支持。",
+          image: _buildImage('wecome_1.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "设计理由",
+          body:
+              "懒，懒得关机等，用一个服务端配合手机面板，远程遥控岂不是美哉😪😪",
+          image: _buildImage('wecome_2.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "额外的功能",
+          body:
+              "中间页面设计成了很强的扩展页，相信在遥远的以后，可能增加些新的功能，但应该都趋向于局域网😀。",
+          image: _buildImage('wecome_3.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "让我们开始使用吧",
+          body:
+              "涵涵写这玩意简直是减了寿的，要嗝屁的😭😭",
+          image: _buildImage('wecome_4.png'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back),
+      skip: const Text('跳过', style: TextStyle(fontWeight: FontWeight.w600)),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('完成', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.black87,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+//本地包↓ 第三方包↑
+import 'zhu.dart';
+import 'Function.dart';
+import 'Introduction.dart';
+import 'Startone.dart';
+
+//路由框架
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isFirstLaunch = prefs.getBool('first_launch_one_Zhou') ?? true;
+  if (isFirstLaunch) {
+    await prefs.setBool('first_launch_one_Zhou', false); // Set the value to false
+    runApp(First_launch());
+  } else {
+    runApp(CardApp());
+  }
+}
+
+class CardApp extends StatefulWidget {
+  @override
+  _CardAppState createState() => _CardAppState();
+}
+
+class _CardAppState extends State<CardApp> with AutomaticKeepAliveClientMixin {
+  //第一次使用的介绍 开始
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  int _selectedIndex = 0;
+  PageController _pageController = PageController();
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return MaterialApp(
+      theme: Theme.of(context).copyWith(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.deepPurple,
+        ),
+      ),
+      home: Scaffold(
+        body: PageView.builder(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            switch (index) {
+              case 0:
+                return KeepAlivePage(ZhuPage());
+              case 1:
+                return KeepAlivePage(FunctionList());
+              case 2:
+                return KeepAlivePage(IntroductionPage());
+              default:
+                return Container();
+            }
+          },
+        ),
+        bottomNavigationBar: MoltenBottomNavigationBar(
+          selectedIndex: _selectedIndex,
+          barHeight: 50,
+          domeHeight: 15,
+          domeWidth: 72,
+          domeCircleSize:45,
+          onTabChange: (clickedIndex) {
+            setState(() {
+              _selectedIndex = clickedIndex;
+              _pageController.animateToPage(
+                clickedIndex,
+                duration: Duration(milliseconds: 350),
+                curve: Curves.linear,
+              );
+            });
+          },
+          tabs: [
+            MoltenTab(
+              icon: Icon(Icons.phonelink_ring),
+            ),
+            MoltenTab(
+              icon: Icon(Icons.credit_card),
+              title: Text('功能'),
+            ),
+            MoltenTab(
+              icon: Icon(Icons.person),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class KeepAlivePage extends StatefulWidget {
+  final Widget child;
+  const KeepAlivePage(this.child);
+  @override
+  _KeepAlivePageState createState() => _KeepAlivePageState();
+}
+
+class _KeepAlivePageState extends State<KeepAlivePage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return widget.child;
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '搜索页面',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '主页',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        '个人资料页面',
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
