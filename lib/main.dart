f384258308ac03068943f57def0693511bc48d8b
Jinhan Zhou <53991448+lanzeweie@users.noreply.github.com>
2024-04-10 16:02:23 +0800

    更换主题颜色，添加深色模式

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:molten_navigationbar_flutter/molten_navigationbar_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-//本地包↓ 第三方包↑
 import 'zhu.dart';
 import 'Function.dart';
 import 'Introduction.dart';
 import 'Startone.dart';
+import 'package:flutter/services.dart';
+import 'color.dart';
 
-//路由框架
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -26,158 +25,107 @@ class CardApp extends StatefulWidget {
 }
 
 class _CardAppState extends State<CardApp> with AutomaticKeepAliveClientMixin {
-  //第一次使用的介绍 开始
+  int _selectedIndex = 0;
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  bool isDarkMode = false; // 必须的颜色代码
 
   @override
+  bool get wantKeepAlive => true;
   void initState() {
     super.initState();
   }
 
-  int _selectedIndex = 0;
-  PageController _pageController = PageController();
-  @override
-  bool get wantKeepAlive => true;
-  MaterialColor customColor = MaterialColor(
-    0xFF40356F, // 颜色代码
-    <int, Color>{
-      50: Color(0xFFEAEAF2),
-      100: Color(0xFFB3B3D9),
-      200: Color(0xFF7C7CBF),
-      300: Color(0xFF4545A6),
-      400: Color(0xFF2E2E91),
-      500: Color(0xFF17177C),
-      600: Color(0xFF12126F),
-      700: Color(0xFF0D0D61),
-      800: Color(0xFF080854),
-      900: Color(0xFF030347),
-    },
-  );
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    super.build(context);
+    // 自动颜色主题
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+    isDarkMode = brightness == Brightness.dark; // Update isDarkMode variable
     return MaterialApp(
-      title:"涵涵面板",
-      theme: Theme.of(context).copyWith(
-        //scaffoldBackgroundColor: Colors.transparent,  //所有页面的背景颜色
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: customColor, // 设置主题颜色为自定义颜色
-        ),
-      ),
+      title: '涵涵面板',
+      theme: isDarkMode
+          ? ThemeData.dark().copyWith(primaryColor: darkColor_AppBar_zhu)
+          : ThemeData.light().copyWith(primaryColor: lightColor_AppBar_zhu),
       home: Scaffold(
-        body: PageView.builder(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          itemCount: 3,
-          itemBuilder: (context, index) {
-            switch (index) {
-              case 0:
-                return KeepAlivePage(ZhuPage());
-              case 1:
-                return KeepAlivePage(FunctionList());
-              case 2:
-                return KeepAlivePage(IntroductionPage());
-              default:
-                return Container();
+        body: WillPopScope(
+          onWillPop: () async {
+            if (_navigatorKey.currentState!.canPop()) {
+              _navigatorKey.currentState!.pop();
+              return false;
+            } else {
+              // 如果当前页面无法返回，则调用系统的返回按钮事件处理方法
+              await SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+              return true;
             }
           },
-        ),
-        bottomNavigationBar: Container(
-          color: Colors.transparent,
-          child: MoltenBottomNavigationBar(
-            selectedIndex: _selectedIndex,
-            barHeight: 50,
-            domeHeight: 15,
-            domeWidth: 72,
-            domeCircleSize: 45,
-            onTabChange: (clickedIndex) {
-              setState(() {
-                _selectedIndex = clickedIndex;
-                _pageController.animateToPage(
-                  clickedIndex,
-                  duration: Duration(milliseconds: 128),
-                  curve: Curves.linear,
-                );
-              });
+          child: Navigator(
+            key: _navigatorKey,
+            onGenerateRoute: (settings) {
+              return MaterialPageRoute(
+                builder: (context) => AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  child: _buildScreen(settings.name ?? ''), // Call _buildScreen method here
+                ),
+              );
             },
-            tabs: [
-              MoltenTab(
-                icon: Icon(Icons.phonelink_ring),
-              ),
-              MoltenTab(
-                icon: Icon(Icons.credit_card),
-              ),
-              MoltenTab(
-                icon: Icon(Icons.person),
-              ),
-            ],
           ),
         ),
       ),
     );
   }
-}
 
-class KeepAlivePage extends StatefulWidget {
-  final Widget child;
-  const KeepAlivePage(this.child);
-  @override
-  _KeepAlivePageState createState() => _KeepAlivePageState();
-}
-
-class _KeepAlivePageState extends State<KeepAlivePage>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  bool get wantKeepAlive => true;
-
-  @override
-  Widget build(BuildContext context) {
-    super.build(context);
-    return widget.child;
-  }
-}
-
-class SearchPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '搜索页面',
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '主页',
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        '个人资料页面',
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    );
+  Widget _buildScreen(String routeName) {
+    switch (routeName) {
+      case '/':
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                '涵涵的超级控制面板😀',
+                style: TextStyle(
+                  color: AppColors.colorConfigText(isDarkMode),
+                ),
+              ),
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.list,
+                    color: AppColors.colorConfigText(isDarkMode),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      _navigatorKey.currentState!.context,
+                      MaterialPageRoute(builder: (context) => FunctionList()),
+                    );
+                  },
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.info,
+                    color: AppColors.colorConfigText(isDarkMode),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      _navigatorKey.currentState!.context,
+                      MaterialPageRoute(builder: (context) => IntroductionPage()),
+                    );
+                  },
+                ),
+              ],
+            ),
+            body: ZhuPage(),
+          ),
+        );
+      default:
+        return Container();
+    }
   }
 }
