69c5f00655ae9af56bec303ed8937f2e33231800
LinTongSheng <lanzeweie@foxmail.com>
2025-07-25 10:47:36 +0800

    统一Material Design风格

--- lib/Introduction.dart
+++ lib/Introduction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -27,6 +28,12 @@ class _IntroductionPageState extends State<IntroductionPage> {
   bool isDarkMode = false; 
   bool isDarkMode_force = false;
   
+  // 添加状态管理各个卡片的展开状态
+  bool _isCard1Expanded = false;
+  bool _isCard2Expanded = true;
+  bool _isCard3Expanded = true;
+  bool _isCard4Expanded = false;
+  
   @override
   void initState() {
     super.initState();
@@ -37,7 +44,6 @@ class _IntroductionPageState extends State<IntroductionPage> {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       isDarkMode_force = prefs.getBool('暗黑模式') ?? false;
-      //print("我在个人介绍页面，我的暗黑模式是：$isDarkMode_force");
     });
   }
 
@@ -45,66 +51,76 @@ class _IntroductionPageState extends State<IntroductionPage> {
   Widget build(BuildContext context) {
     // 自动颜色主题
     final Brightness brightness = MediaQuery.of(context).platformBrightness;
-    isDarkMode = brightness == Brightness.dark; // Update isDarkMode variable
+    isDarkMode = brightness == Brightness.dark;
+    
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(45), // 设置顶部栏的高度为 80 像素
-        child: AppBar(
-          title: Text(
-            '涵涵在这里',
-            style: TextStyle(
-              fontSize: 20, // 设置字号为20
-              color: AppColors.colorConfigText(context),
-            ),
-          ),
-          centerTitle: true, // 文字居中显示
-          backgroundColor: AppColors.colorConfigKuangJia(context),
-          iconTheme: IconThemeData(
-            color: AppColors.colorConfigJianTou(context), // 设置返回箭头的颜色
+      backgroundColor: AppColors.colorConfigSwitchGroupBackground(context),
+      appBar: AppBar(
+        backgroundColor: AppColors.colorConfigKuangJia(context),
+        elevation: 0,
+        title: Text(
+          '涵涵在这里',
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: AppColors.colorConfigText(context),
           ),
         ),
+        centerTitle: true,
+        iconTheme: IconThemeData(
+          color: AppColors.colorConfigJianTou(context),
+        ),
+        systemOverlayStyle: isDarkMode ? SystemUiOverlayStyle.light : SystemUiOverlayStyle.dark,
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ProfileHeader(), // 添加头像框
+            _buildProfileHeader(),
             Padding(
-              padding: EdgeInsets.all(16.0),
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
               child: Column(
                 children: [
-                  AnimatedCard(
+                  _buildInfoCard(
                     title: '涵涵的命令面板',
                     description: '这是信息块 1 的详细描述。\n这是第二行。',
+                    icon: Icons.dashboard,
                     color: Colors.blue,
                     isExpandable: false,
-                    isExpandedByDefault: false, // 默认展开
-                    cardIcon: Icons.star, // 添加卡片图标
+                    isExpanded: _isCard1Expanded,
+                    onToggle: (expanded) => setState(() => _isCard1Expanded = expanded),
                   ),
-                  AnimatedCard(
+                  SizedBox(height: 8),
+                  _buildInfoCard(
                     title: '设计原因',
                     description: '在移动端的快捷面板可以方便一些命令的实施\n简易快捷的与PC服务端进行对接\n快速进行命令交互',
+                    icon: Icons.lightbulb,
                     color: Colors.green,
-                    isExpandable: false, // 禁止收回
-                    isExpandedByDefault: true, // 默认展开
-                    cardIcon: Icons.favorite, // 添加卡片图标
+                    isExpandable: false,
+                    isExpanded: _isCard2Expanded,
+                    onToggle: (expanded) => setState(() => _isCard2Expanded = expanded),
                   ),
-                  AnimatedCard(
+                  SizedBox(height: 8),
+                  _buildInfoCard(
                     title: '开发者信息',
                     description: '邮箱：lanzeweie@foxmail.com\nQ Q：449003810',
+                    icon: Icons.person,
                     color: Colors.orange,
                     isExpandable: true,
-                    isExpandedByDefault: true, // 默认展开
-                    cardIcon: Icons.wechat, // 添加卡片图标
+                    isExpanded: _isCard3Expanded,
+                    onToggle: (expanded) => setState(() => _isCard3Expanded = expanded),
                   ),
-                  AnimatedCard(
+                  SizedBox(height: 8),
+                  _buildInfoCard(
                     title: '开源 [2023/8/26]',
                     description: '语言：Dart\n框架：Flutter\n开源：Github',
-                    color: Colors.orange,
+                    icon: Icons.code,
+                    color: Colors.purple,
                     isExpandable: true,
-                    isExpandedByDefault: false, // 默认展开
+                    isExpanded: _isCard4Expanded,
+                    onToggle: (expanded) => setState(() => _isCard4Expanded = expanded),
                     url: 'https://github.com/lanzeweie/HanHan',
-                    cardIcon: Icons.link, // 添加卡片图标
                   ),
+                  SizedBox(height: 16),
                 ],
               ),
             ),
@@ -113,120 +129,199 @@ class _IntroductionPageState extends State<IntroductionPage> {
       ),
     );
   }
-}
 
-class ProfileHeader extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildProfileHeader() {
     return Container(
       padding: EdgeInsets.all(16.0),
-      color: Colors.transparent,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            radius: 40.0,
-            backgroundImage: NetworkImage('https://q1.qlogo.cn/g?b=qq&nk=449003810&s=640'), // 替换成您的图片链接
+      child: Card(
+        elevation: 2,
+        shadowColor: isDarkMode ? Colors.black26 : Colors.black12,
+        color: AppColors.colorConfigKuangJia(context),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.commandApiElement(context),
+                    width: 2,
+                  ),
+                ),
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage('https://q1.qlogo.cn/g?b=qq&nk=449003810&s=640'),
+                  backgroundColor: Colors.grey[300],
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '涵涵开发者',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.colorConfigText(context),
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'Flutter & Dart Developer',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.colorConfigSettilteText(isDarkMode_force, isDarkMode),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-}
 
-class AnimatedCard extends StatefulWidget {
-  final String title;
-  final String description;
-  final Color color;
-  final bool isExpandable;
-  final bool isExpandedByDefault; // 新增属性
-  final String? url;
-  final IconData cardIcon;
-
-  AnimatedCard({
-    required this.title,
-    required this.description,
-    required this.color,
-    required this.isExpandable,
-    this.isExpandedByDefault = false, // 默认值为false
-    this.url,
-    required this.cardIcon,
-  });
-
-  @override
-  _AnimatedCardState createState() => _AnimatedCardState();
-}
-
-class _AnimatedCardState extends State<AnimatedCard> {
-  late bool _isExpanded;
-
-  @override
-  void initState() {
-    super.initState();
-    _isExpanded = widget.isExpandedByDefault;
-  }
-
-  void _toggleExpansion() {
-    if (widget.isExpandable) {
-      setState(() {
-        _isExpanded = !_isExpanded;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8.0),
-      decoration: BoxDecoration(
-        color: widget.color,
-        borderRadius: BorderRadius.circular(10.0),
+  Widget _buildInfoCard({
+    required String title,
+    required String description,
+    required IconData icon,
+    required Color color,
+    required bool isExpandable,
+    required bool isExpanded,
+    required Function(bool) onToggle,
+    String? url,
+  }) {
+    return Card(
+      elevation: 2,
+      shadowColor: isDarkMode ? Colors.black26 : Colors.black12,
+      color: AppColors.colorConfigKuangJia(context),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ListTile(
-            onTap: _toggleExpansion,
-            contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-            leading: Icon(widget.cardIcon, color: Colors.white),
-            title: Text(widget.title, style: TextStyle(color: Colors.white)),
-            trailing: widget.isExpandable
-                ? _isExpanded
-                    ? Icon(Icons.expand_less, color: Colors.white)
-                    : Icon(Icons.expand_more, color: Colors.white)
-                : null,
-          ),
-          if (_isExpanded)
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0), // 添加垂直间距
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.description,
-                    style: TextStyle(color: Colors.white),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: color.withOpacity(0.05),
+              ),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                onTap: isExpandable ? () => onToggle(!isExpanded) : null,
+                leading: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: color.withOpacity(0.15),
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  if (widget.url != null)
-                    GestureDetector(
-                      onTap: () => launch(widget.url!),
-                      child: Row(
-                        children: [
-                          Icon(Icons.link, color: Colors.white, size: 16),
-                          SizedBox(width: 4),
-                          Text(
-                            '点击查看链接',
-                            style: TextStyle(
-                              color: Colors.white,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ],
+                  child: Icon(
+                    icon,
+                    color: color,
+                    size: 20,
+                  ),
+                ),
+                title: Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.colorConfigText(context),
+                  ),
+                ),
+                trailing: isExpandable 
+                  ? Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: AppColors.colorConfigIcon(isDarkMode_force, isDarkMode).withOpacity(0.1),
+                        borderRadius: BorderRadius.circular(6),
                       ),
-                    ),
-                ],
+                      child: Icon(
+                        isExpanded ? Icons.expand_less : Icons.expand_more,
+                        color: AppColors.colorConfigIcon(isDarkMode_force, isDarkMode),
+                        size: 18,
+                      ),
+                    )
+                  : null,
               ),
             ),
-        ],
+            AnimatedCrossFade(
+              firstChild: Container(),
+              secondChild: Container(
+                width: double.infinity,
+                padding: EdgeInsets.fromLTRB(20, 16, 20, 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      description,
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: AppColors.colorConfigSettilteText(isDarkMode_force, isDarkMode),
+                        height: 1.6,
+                      ),
+                    ),
+                    if (url != null) ...[
+                      SizedBox(height: 16),
+                      Material(
+                        color: Colors.transparent,
+                        child: InkWell(
+                          onTap: () => launch(url),
+                          borderRadius: BorderRadius.circular(8),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: color.withOpacity(0.08),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(
+                                  Icons.open_in_new,
+                                  color: color,
+                                  size: 16,
+                                ),
+                                SizedBox(width: 8),
+                                Text(
+                                  '访问Github仓库',
+                                  style: TextStyle(
+                                    color: color,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 13,
+                                  ),
+                                ),
+                                SizedBox(width: 6),
+                                Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: color,
+                                  size: 10,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
+              crossFadeState: isExpanded 
+                ? CrossFadeState.showSecond 
+                : CrossFadeState.showFirst,
+              duration: Duration(milliseconds: 250),
+              sizeCurve: Curves.easeInOut,
+            ),
+          ],
+        ),
       ),
     );
   }
